c7bcd7d196795850c923b808f91b92af267287a9
wira07 <wiralodrasaputra07@gmail.com>
2023-11-07 14:46:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/learning_app/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.learning_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY = 5000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
