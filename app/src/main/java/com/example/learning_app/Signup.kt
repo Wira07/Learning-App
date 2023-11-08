ace3116ada5a786992b0c229bc7ad549541a6c06
wira07 <wiralodrasaputra07@gmail.com>
2023-11-08 22:38:55 +0700

    update file baru

---
+++ app/src/main/java/com/example/learning_app/Signup.kt
@@ -0,0 +1,11 @@
+package com.example.learning_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
--- app/src/main/java/com/example/learning_app/Slicing.kt
+++ app/src/main/java/com/example/learning_app/Slicing.kt
@@ -1,11 +1,22 @@
 package com.example.learning_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.learning_app.databinding.ActivitySlicingBinding
 
 class Slicing : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySlicingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_slicing)
+        binding = ActivitySlicingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            intent.putExtra("kunci", "Nilai yang akan Anda kirim")
+            startActivity(intent)
+        }
     }
 }
