71f9e5717a437e22a8fece97e696383bd9c1c682
wira07 <wiralodrasaputra07@gmail.com>
2023-11-09 17:27:51 +0700

    update file baru

--- app/src/main/java/com/example/learning_app/Signup.kt
+++ app/src/main/java/com/example/learning_app/Signup.kt
@@ -1,11 +1,23 @@
 package com.example.learning_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.learning_app.databinding.ActivitySignUpBinding
 
 class Signup : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("kunci", "Nilai yang akan Anda kirim")
+            startActivity(intent)
+        }
     }
 }
